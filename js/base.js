a747b9f536755c5e4b6287fd43e60b0c8917b382
secondarykey <secondarykey@gmail.clm>
2012-07-21 16:48:03 +0900

    基本的な部分を追加

--- js/base.js
+++ js/base.js
@@ -1,5 +1,16 @@
 $(function() {
 	$(document).ready(function(){
+		$.jmpress("template", "auto", {
+			children: function(idx) {
+				return {
+					y: 400
+					,x: -300 + idx * 300
+					,template: "auto"
+					,scale: 0.3
+				}
+			}
+		});
+
 		$('#jmpress').jmpress();
 	});
 });
