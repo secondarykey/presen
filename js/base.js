ccb84ae7e2690ffabc234589820ae9aa5c186784
secondarykey <secondarykey@gmail.com>
2012-08-01 15:34:55 +0900

    プレゼンのたたき台を作成

--- js/base.js
+++ js/base.js
@@ -3,14 +3,17 @@ $(function() {
 		$.jmpress("template", "auto", {
 			children: function(idx) {
 				return {
-					y: 400
-					,x: -300 + idx * 300
-					,template: "auto"
-					,scale: 0.3
-				}
+					y: 400 ,
+					x: -300 + idx * 300 ,
+					template: "auto" ,
+					scale: 0.3
+				};
 			}
 		});
 
-		$('#jmpress').jmpress();
+		$('#jmpress').jmpress({
+			stepSelector: 'section'		,hash: { use: false }
+
+		});
 	});
 });
